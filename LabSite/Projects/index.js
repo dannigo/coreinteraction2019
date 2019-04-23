f4398fda8a2cc88e5de7db39f00899bcc50de3bf
dannigo <46946133+dannigo@users.noreply.github.com>
2019-04-23 18:57:20 -0400

    Add files via upload

--- LabSite/Projects/index.js
+++ LabSite/Projects/index.js
@@ -1,38 +1,335 @@
 $(document).ready(function () {
 
+    //DRAG//
     $(".passDrag").draggable({
         cursor: "grabbing"
         });
 
-    $('#exitVids').click(function(){
+
+    //EXIT//
+    $('#exitVids.2017Sept1').click(function(){
         let clickHide = 'hidden';
 
-        $('#tripVids').css('visibility', clickHide);   
+        $('#tripVids.2017Sept1').css('visibility', clickHide);   
     });
 
-    $('#exitPhotos').click(function(){
+    $('#exitPhotos.2017Sept2').click(function(){
         let clickHide = 'hidden';
 
-        $('#tripPhotos').css('visibility', clickHide);   
+        $('#tripPhotos.2017Sept2').css('visibility', clickHide);   
+    });
+
+    $('#exitPhotos.2017Sept3').click(function(){
+        let clickHide = 'hidden';
+
+        $('#tripPhotos.2017Sept3').css('visibility', clickHide);   
+    });
+
+    $('#exitPhotos.2017Sept4').click(function(){
+        let clickHide = 'hidden';
+
+        $('#tripPhotos.2017Sept4').css('visibility', clickHide);   
+    });
+
+    $('#exitVids.2017Nov1').click(function(){
+        let clickHide = 'hidden';
+
+        $('#tripVids.2017Nov1').css('visibility', clickHide);   
+    });
+
+    $('#exitPhotos.2017Nov2').click(function(){
+        let clickHide = 'hidden';
+
+        $('#tripPhotos.2017Nov2').css('visibility', clickHide);   
+    });
+
+    $('#exitPhotos.2017Nov3').click(function(){
+        let clickHide = 'hidden';
+
+        $('#tripPhotos.2017Nov3').css('visibility', clickHide);   
+    });
+
+    $('#exitPhotos.2018Jan1').click(function(){
+        let clickHide = 'hidden';
+
+        $('#tripPhotos.2018Jan1').css('visibility', clickHide);   
+    });
+
+    $('#exitVids.2018Jan1').click(function(){
+        let clickHide = 'hidden';
+
+        $('#tripVids.2018Jan1').css('visibility', clickHide);   
+    });
+
+    $('#exitVids.2018Jan2').click(function(){
+        let clickHide = 'hidden';
+
+        $('#tripVids.2018Jan2').css('visibility', clickHide);   
+    });
+
+    $('#exitVids.2018Feb1').click(function(){
+        let clickHide = 'hidden';
+
+        $('#tripVids.2018Feb1').css('visibility', clickHide);   
+    });
+
+    $('#exitVids.2018Feb2').click(function(){
+        let clickHide = 'hidden';
+
+        $('#tripVids.2018Feb2').css('visibility', clickHide);   
+    });
+
+    $('#exitVids.2018Feb3').click(function(){
+        let clickHide = 'hidden';
+
+        $('#tripVids.2018Feb3').css('visibility', clickHide);   
+    });
+
+    $('#exitPhotos.2018Mar1').click(function(){
+        let clickHide = 'hidden';
+
+        $('#tripPhotos.2018Mar1').css('visibility', clickHide);   
+    });
+
+    $('#exitPhotos.2018Mar2').click(function(){
+        let clickHide = 'hidden';
+
+        $('#tripPhotos.2018Mar2').css('visibility', clickHide);   
+    });
+
+    $('#exitPhotos.2018Mar3').click(function(){
+        let clickHide = 'hidden';
+
+        $('#tripPhotos.2018Mar3').css('visibility', clickHide);   
+    });
+
+    $('#exitPhotos.2018Apr1').click(function(){
+        let clickHide = 'hidden';
+
+        $('#tripPhotos.2018Apr1').css('visibility', clickHide);   
+    });
+
+    $('#exitVids.2018Apr2').click(function(){
+        let clickHide = 'hidden';
+
+        $('#tripVids.2018Apr2').css('visibility', clickHide);   
+    });
+
+    $('#exitVids.2018Aug1').click(function(){
+        let clickHide = 'hidden';
+
+        $('#tripVids.2018Aug1').css('visibility', clickHide);   
+    });
+
+    $('#exitVids.2018Aug2').click(function(){
+        let clickHide = 'hidden';
+
+        $('#tripVids.2018Aug2').css('visibility', clickHide);   
+    });
+
+    $('#exitVids.2018Sept1').click(function(){
+        let clickHide = 'hidden';
+
+        $('#tripVids.2018Sept1').css('visibility', clickHide);   
+    });
+
+    $('#exitVids.2018Sept2').click(function(){
+        let clickHide = 'hidden';
+
+        $('#tripVids.2018Sept2').css('visibility', clickHide);   
     });
  
+    $('#exitVids.2018Nov1').click(function(){
+        let clickHide = 'hidden';
+
+        $('#tripVids.2018Nov1').css('visibility', clickHide);   
+    });
+
+    $('#exitPhotos.2018Nov2').click(function(){
+        let clickHide = 'hidden';
+
+        $('#tripPhotos.2018Nov2').css('visibility', clickHide);   
+    });
+
+    $('#exitPhotos.2019Jan').click(function(){
+        let clickHide = 'hidden';
+
+        $('#tripPhotos.2019Jan').css('visibility', clickHide);   
+    });
+
+    $('#exitPhotos.2019Mar1').click(function(){
+        let clickHide = 'hidden';
+
+        $('#tripPhotos.2019Mar1').css('visibility', clickHide);   
+    });
+
+    $('#exitPhotos.2019Mar2').click(function(){
+        let clickHide = 'hidden';
+
+        $('#tripPhotos.2019Mar2').css('visibility', clickHide);   
+    });
+
+
+    
+    
+    //PASS POP-UP//
     $('#pass1').dblclick(function(){
         let dblShow = 'visible';
 
-        $('#tripVids').css('visibility', dblShow);   
+        $('#tripVids.2017Sept1').css('visibility', dblShow);   
     });
 
     $('#pass2').dblclick(function(){
         let dblShow = 'visible';
 
-        $('#tripPhotos').css('visibility', dblShow);   
+        $('#tripPhotos.2017Sept2').css('visibility', dblShow);   
     });
 
-    // $('#Sept21b').click(function(){
-    //     let clickPic = 'visible';
+    $('#pass3').dblclick(function(){
+        let dblShow = 'visible';
 
-    //     $('.Sept21pic1').css('visibility', clickPic);   
-    // });
+        $('#tripPhotos.2017Sept3').css('visibility', dblShow);   
+    });
+
+    $('#pass4').dblclick(function(){
+        let dblShow = 'visible';
+
+        $('#tripPhotos.2017Sept4').css('visibility', dblShow);   
+    });
+
+    $('#pass5').dblclick(function(){
+        let dblShow = 'visible';
+
+        $('#tripVids.2017Nov1').css('visibility', dblShow);   
+    });
+
+    $('#pass6').dblclick(function(){
+        let dblShow = 'visible';
+
+        $('#tripPhotos.2017Nov2').css('visibility', dblShow);   
+    });
+
+    $('#pass7').dblclick(function(){
+        let dblShow = 'visible';
+
+        $('#tripPhotos.2017Nov3').css('visibility', dblShow);   
+    });
+
+    $('#pass8').dblclick(function(){
+        let dblShow = 'visible';
+
+        $('#tripVids.2018Jan1').css('visibility', dblShow);   
+    });
+
+    $('#pass9').dblclick(function(){
+        let dblShow = 'visible';
+
+        $('#tripVids.2018Jan2').css('visibility', dblShow);   
+    });
+
+    $('#pass10').dblclick(function(){
+        let dblShow = 'visible';
+
+        $('#tripVids.2018Feb1').css('visibility', dblShow);   
+    });
+
+    $('#pass11').dblclick(function(){
+        let dblShow = 'visible';
+
+        $('#tripVids.2018Feb2').css('visibility', dblShow);   
+    });
+
+    $('#pass12').dblclick(function(){
+        let dblShow = 'visible';
+
+        $('#tripVids.2018Feb3').css('visibility', dblShow);   
+    });
+
+    $('#pass13').dblclick(function(){
+        let dblShow = 'visible';
+
+        $('#tripPhotos.2018Mar1').css('visibility', dblShow);   
+    });
+
+    $('#pass14').dblclick(function(){
+        let dblShow = 'visible';
+
+        $('#tripPhotos.2018Mar2').css('visibility', dblShow);   
+    });
+
+    $('#pass15').dblclick(function(){
+        let dblShow = 'visible';
+
+        $('#tripPhotos.2018Mar3').css('visibility', dblShow);   
+    });
+
+    $('#pass16').dblclick(function(){
+        let dblShow = 'visible';
+
+        $('#tripPhotos.2018Apr1').css('visibility', dblShow);   
+    });
+
+    $('#pass17').dblclick(function(){
+        let dblShow = 'visible';
+
+        $('#tripVids.2018Apr2').css('visibility', dblShow);   
+    });
+
+    $('#pass18').dblclick(function(){
+        let dblShow = 'visible';
+
+        $('#tripVids.2018Aug1').css('visibility', dblShow);   
+    });
+
+    $('#pass19').dblclick(function(){
+        let dblShow = 'visible';
+
+        $('#tripVids.2018Aug2').css('visibility', dblShow);   
+    });
+
+    $('#pass20').dblclick(function(){
+        let dblShow = 'visible';
+
+        $('#tripVids.2018Sept1').css('visibility', dblShow);   
+    });
+
+    $('#pass21').dblclick(function(){
+        let dblShow = 'visible';
+
+        $('#tripVids.2018Sept2').css('visibility', dblShow);   
+    });
+
+    $('#pass22').dblclick(function(){
+        let dblShow = 'visible';
+
+        $('#tripVids.2018Nov1').css('visibility', dblShow);   
+    });
+
+    $('#pass23').dblclick(function(){
+        let dblShow = 'visible';
+
+        $('#tripPhotos.2018Nov2').css('visibility', dblShow);   
+    });
+
+    $('#pass24').dblclick(function(){
+        let dblShow = 'visible';
+
+        $('#tripPhotos.2019Jan').css('visibility', dblShow);   
+    });
+
+    $('#pass25').dblclick(function(){
+        let dblShow = 'visible';
+
+        $('#tripPhotos.2019Mar1').css('visibility', dblShow);   
+    });
+
+    $('#pass26').dblclick(function(){
+        let dblShow = 'visible';
+
+        $('#tripPhotos.2019Mar2').css('visibility', dblShow);   
+    });
+
+
 
         
 });
