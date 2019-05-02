f28dee7c59da15e5bf1802a0650fd3303f7d8103
dannigo <46946133+dannigo@users.noreply.github.com>
2019-05-02 19:49:24 -0400

    Add files via upload

--- StudioSite/Projects/Project2/script.js
+++ StudioSite/Projects/Project2/script.js
@@ -4,16 +4,23 @@ $(document).ready(function(){
 
     let wordsTwo = ['LOSS', 'LOST', 'HERE', 'FLUX', 'SOLO', 'ECHO', 'BEST'];
 
+
     let count = 0;
 
     function randomWordsOne() {
+
+        // let randomizeOne = wordsOne[ Math.floor(Math.random() * wordsOne.length) ];
+
         $("#self").text(wordsOne[count]);
         count < 7 ? count++ : count = 0;
     }
 
     setInterval(randomWordsOne, 150);
 
-    let count2 = 0;
+    let count2 = 7;
+
+        // let randomizeTwo = wordsOne[ Math.floor(Math.random() * wordsOne.length) ];
+
         function randomWordsTwo() {
             $("#loss").text(wordsTwo[count2]);
             count2 < 7 ? count2++ : count2 = 0;
