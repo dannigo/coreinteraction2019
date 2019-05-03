c1a47c02e7b431d34252d00cdae09a4fe7755544
dannigo <46946133+dannigo@users.noreply.github.com>
2019-05-02 21:33:56 -0400

    Add files via upload

--- LabSite/Projects/index.js
+++ LabSite/Projects/index.js
@@ -1,335 +1,44 @@
-$(document).ready(function () {
+$(document).ready(function() {
 
     //DRAG//
     $(".passDrag").draggable({
         cursor: "grabbing"
         });
 
-
-    //EXIT//
-    $('#exitVids.2017Sept1').click(function(){
-        let clickHide = 'hidden';
-
-        $('#tripVids.2017Sept1').css('visibility', clickHide);   
+    //EXIT PASS POP-UP//
+    $('.close').click(function(){
+        //You need to change the letiable $popup to make it equal the popup that the close button is inside of
+        //Lookup the .parents() method on this page https://learn.jquery.com/using-jquery-core/traversing/
+        //Remember, $(this) will equal the .close div inside of the .popup
+        $popup = $(this).parent();
+        //This will create an error while $popup = null
+        $popup.removeClass('open');
     });
-
-    $('#exitPhotos.2017Sept2').click(function(){
-        let clickHide = 'hidden';
-
-        $('#tripPhotos.2017Sept2').css('visibility', clickHide);   
-    });
-
-    $('#exitPhotos.2017Sept3').click(function(){
-        let clickHide = 'hidden';
-
-        $('#tripPhotos.2017Sept3').css('visibility', clickHide);   
-    });
-
-    $('#exitPhotos.2017Sept4').click(function(){
-        let clickHide = 'hidden';
-
-        $('#tripPhotos.2017Sept4').css('visibility', clickHide);   
-    });
-
-    $('#exitVids.2017Nov1').click(function(){
-        let clickHide = 'hidden';
-
-        $('#tripVids.2017Nov1').css('visibility', clickHide);   
-    });
-
-    $('#exitPhotos.2017Nov2').click(function(){
-        let clickHide = 'hidden';
-
-        $('#tripPhotos.2017Nov2').css('visibility', clickHide);   
-    });
-
-    $('#exitPhotos.2017Nov3').click(function(){
-        let clickHide = 'hidden';
-
-        $('#tripPhotos.2017Nov3').css('visibility', clickHide);   
-    });
-
-    $('#exitPhotos.2018Jan1').click(function(){
-        let clickHide = 'hidden';
-
-        $('#tripPhotos.2018Jan1').css('visibility', clickHide);   
-    });
-
-    $('#exitVids.2018Jan1').click(function(){
-        let clickHide = 'hidden';
-
-        $('#tripVids.2018Jan1').css('visibility', clickHide);   
-    });
-
-    $('#exitVids.2018Jan2').click(function(){
-        let clickHide = 'hidden';
-
-        $('#tripVids.2018Jan2').css('visibility', clickHide);   
-    });
-
-    $('#exitVids.2018Feb1').click(function(){
-        let clickHide = 'hidden';
-
-        $('#tripVids.2018Feb1').css('visibility', clickHide);   
-    });
-
-    $('#exitVids.2018Feb2').click(function(){
-        let clickHide = 'hidden';
-
-        $('#tripVids.2018Feb2').css('visibility', clickHide);   
-    });
-
-    $('#exitVids.2018Feb3').click(function(){
-        let clickHide = 'hidden';
-
-        $('#tripVids.2018Feb3').css('visibility', clickHide);   
-    });
-
-    $('#exitPhotos.2018Mar1').click(function(){
-        let clickHide = 'hidden';
-
-        $('#tripPhotos.2018Mar1').css('visibility', clickHide);   
-    });
-
-    $('#exitPhotos.2018Mar2').click(function(){
-        let clickHide = 'hidden';
-
-        $('#tripPhotos.2018Mar2').css('visibility', clickHide);   
-    });
-
-    $('#exitPhotos.2018Mar3').click(function(){
-        let clickHide = 'hidden';
-
-        $('#tripPhotos.2018Mar3').css('visibility', clickHide);   
-    });
-
-    $('#exitPhotos.2018Apr1').click(function(){
-        let clickHide = 'hidden';
-
-        $('#tripPhotos.2018Apr1').css('visibility', clickHide);   
-    });
-
-    $('#exitVids.2018Apr2').click(function(){
-        let clickHide = 'hidden';
-
-        $('#tripVids.2018Apr2').css('visibility', clickHide);   
-    });
-
-    $('#exitVids.2018Aug1').click(function(){
-        let clickHide = 'hidden';
-
-        $('#tripVids.2018Aug1').css('visibility', clickHide);   
-    });
-
-    $('#exitVids.2018Aug2').click(function(){
-        let clickHide = 'hidden';
-
-        $('#tripVids.2018Aug2').css('visibility', clickHide);   
-    });
-
-    $('#exitVids.2018Sept1').click(function(){
-        let clickHide = 'hidden';
-
-        $('#tripVids.2018Sept1').css('visibility', clickHide);   
-    });
-
-    $('#exitVids.2018Sept2').click(function(){
-        let clickHide = 'hidden';
-
-        $('#tripVids.2018Sept2').css('visibility', clickHide);   
-    });
- 
-    $('#exitVids.2018Nov1').click(function(){
-        let clickHide = 'hidden';
-
-        $('#tripVids.2018Nov1').css('visibility', clickHide);   
-    });
-
-    $('#exitPhotos.2018Nov2').click(function(){
-        let clickHide = 'hidden';
-
-        $('#tripPhotos.2018Nov2').css('visibility', clickHide);   
-    });
-
-    $('#exitPhotos.2019Jan').click(function(){
-        let clickHide = 'hidden';
-
-        $('#tripPhotos.2019Jan').css('visibility', clickHide);   
-    });
-
-    $('#exitPhotos.2019Mar1').click(function(){
-        let clickHide = 'hidden';
-
-        $('#tripPhotos.2019Mar1').css('visibility', clickHide);   
-    });
-
-    $('#exitPhotos.2019Mar2').click(function(){
-        let clickHide = 'hidden';
-
-        $('#tripPhotos.2019Mar2').css('visibility', clickHide);   
-    });
-
 
     
-    
-    //PASS POP-UP//
-    $('#pass1').dblclick(function(){
-        let dblShow = 'visible';
-
-        $('#tripVids.2017Sept1').css('visibility', dblShow);   
-    });
-
-    $('#pass2').dblclick(function(){
-        let dblShow = 'visible';
-
-        $('#tripPhotos.2017Sept2').css('visibility', dblShow);   
-    });
-
-    $('#pass3').dblclick(function(){
-        let dblShow = 'visible';
-
-        $('#tripPhotos.2017Sept3').css('visibility', dblShow);   
-    });
-
-    $('#pass4').dblclick(function(){
-        let dblShow = 'visible';
-
-        $('#tripPhotos.2017Sept4').css('visibility', dblShow);   
-    });
-
-    $('#pass5').dblclick(function(){
-        let dblShow = 'visible';
-
-        $('#tripVids.2017Nov1').css('visibility', dblShow);   
-    });
-
-    $('#pass6').dblclick(function(){
-        let dblShow = 'visible';
-
-        $('#tripPhotos.2017Nov2').css('visibility', dblShow);   
-    });
-
-    $('#pass7').dblclick(function(){
-        let dblShow = 'visible';
-
-        $('#tripPhotos.2017Nov3').css('visibility', dblShow);   
-    });
-
-    $('#pass8').dblclick(function(){
-        let dblShow = 'visible';
-
-        $('#tripVids.2018Jan1').css('visibility', dblShow);   
-    });
-
-    $('#pass9').dblclick(function(){
-        let dblShow = 'visible';
-
-        $('#tripVids.2018Jan2').css('visibility', dblShow);   
-    });
-
-    $('#pass10').dblclick(function(){
-        let dblShow = 'visible';
-
-        $('#tripVids.2018Feb1').css('visibility', dblShow);   
-    });
-
-    $('#pass11').dblclick(function(){
-        let dblShow = 'visible';
-
-        $('#tripVids.2018Feb2').css('visibility', dblShow);   
-    });
-
-    $('#pass12').dblclick(function(){
-        let dblShow = 'visible';
-
-        $('#tripVids.2018Feb3').css('visibility', dblShow);   
-    });
-
-    $('#pass13').dblclick(function(){
-        let dblShow = 'visible';
-
-        $('#tripPhotos.2018Mar1').css('visibility', dblShow);   
-    });
-
-    $('#pass14').dblclick(function(){
-        let dblShow = 'visible';
-
-        $('#tripPhotos.2018Mar2').css('visibility', dblShow);   
-    });
-
-    $('#pass15').dblclick(function(){
-        let dblShow = 'visible';
-
-        $('#tripPhotos.2018Mar3').css('visibility', dblShow);   
-    });
-
-    $('#pass16').dblclick(function(){
-        let dblShow = 'visible';
-
-        $('#tripPhotos.2018Apr1').css('visibility', dblShow);   
-    });
-
-    $('#pass17').dblclick(function(){
-        let dblShow = 'visible';
-
-        $('#tripVids.2018Apr2').css('visibility', dblShow);   
-    });
-
-    $('#pass18').dblclick(function(){
-        let dblShow = 'visible';
-
-        $('#tripVids.2018Aug1').css('visibility', dblShow);   
-    });
-
-    $('#pass19').dblclick(function(){
-        let dblShow = 'visible';
-
-        $('#tripVids.2018Aug2').css('visibility', dblShow);   
-    });
-
-    $('#pass20').dblclick(function(){
-        let dblShow = 'visible';
-
-        $('#tripVids.2018Sept1').css('visibility', dblShow);   
-    });
-
-    $('#pass21').dblclick(function(){
-        let dblShow = 'visible';
-
-        $('#tripVids.2018Sept2').css('visibility', dblShow);   
-    });
-
-    $('#pass22').dblclick(function(){
-        let dblShow = 'visible';
-
-        $('#tripVids.2018Nov1').css('visibility', dblShow);   
-    });
-
-    $('#pass23').dblclick(function(){
-        let dblShow = 'visible';
-
-        $('#tripPhotos.2018Nov2').css('visibility', dblShow);   
-    });
-
-    $('#pass24').dblclick(function(){
-        let dblShow = 'visible';
-
-        $('#tripPhotos.2019Jan').css('visibility', dblShow);   
-    });
-
-    $('#pass25').dblclick(function(){
-        let dblShow = 'visible';
-
-        $('#tripPhotos.2019Mar1').css('visibility', dblShow);   
-    });
-
-    $('#pass26').dblclick(function(){
-        let dblShow = 'visible';
-
-        $('#tripPhotos.2019Mar2').css('visibility', dblShow);   
+    //OPEN PASS POP-UP//
+    $('.passDrag').dblclick(function(){
+        let popupID = $(this).attr('data-id');
+        console.log('popupID:' + popupID);
+        //Make $popup equal the .popup with the ID referenced in popupID.
+        $popup = $('.popup#' + popupID)  ;
+        //This will create an error while $popup = null
+        $popup.addClass('open');
     });
 
 
 
+    let topZ = 10;
+    //BRING TO FRONT ON CLICK
+    $('.passDrag').click(function(){
+
+        //I'm changing this because there was no way to bring a pass in front of one that already
+        //been clicked. Eventually they will all be at 100001. This creates the topZ OUTSIDE of the function
+        //and adds to it everytime you click a pass, that way the number is always increasing
+        topZ = topZ + 1 ;
+        $(this).css('z-index', topZ);
+        //Open the browser console and you can see this number growing
+        // console.log('New z-index:'+topZ);
+    });
         
 });
